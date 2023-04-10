5058e80bacd5b8eed5da1f500bda8fdccabfbff9
harshil0073 <128502948+harshil0073@users.noreply.github.com>
2023-04-10 12:29:31 -0400

    updated

--- gesture_App/gesture_App/ViewController.swift
+++ gesture_App/gesture_App/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var tapCount = 0
+    
+    @IBOutlet var myView: UIView!
+    @IBOutlet var tapCountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(_:)))
+        tapGestureRecognizer.numberOfTapsRequired = 2
+        myView.addGestureRecognizer(tapGestureRecognizer)
     }
 
-
+   
+    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
+        if sender.numberOfTapsRequired == 1 {
+            tapCount+=1
+            tapCountLabel.text = "\(tapCount)"
+        }
+        else if(sender.numberOfTapsRequired == 2){
+            tapCount = 0
+            tapCountLabel.text = "\(tapCount)"
+            print("Double Tap")
+            self.performSegue(withIdentifier: "showSecond", sender: self)
+        }
+    }
+    
+    @IBAction func didLongPress(_ sender: UILongPressGestureRecognizer) {
+        
+    }
+    
+    @IBAction func didPanGesture(_ sender: UIPanGestureRecognizer) {
+    }
 }
 
